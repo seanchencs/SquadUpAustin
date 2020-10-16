177a75387e5ed1debb3c57c53dbf0a7a2c20f561
Sean Chen <sean.chen@utexas.edu>
2020-10-16 13:42:03 -0500

    Initial Commit

---
+++ SquadUpAustin/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SquadUpAustin
+//
+//  Created by Sean Chen on 10/16/20.
+//  Copyright © 2020 Group 4. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
