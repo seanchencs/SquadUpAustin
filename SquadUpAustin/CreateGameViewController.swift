ddbed15dbcfa2cfff3a75f06c6e67e88ec2b05d3
Reagan Lasswell <reagancl@zoe.cs.utexas.edu>
2020-10-21 17:16:52 -0500

    Added A create game and select game view controller. Also added some logic to main view controller

---
+++ SquadUpAustin/CreateGameViewController.swift
@@ -0,0 +1,22 @@
+//
+//  CreateGameViewController.swift
+//  SquadUpAustin
+//
+//  Created by Reagan Lasswell on 10/21/20.
+//  Copyright © 2020 Group 4. All rights reserved.
+//
+
+import UIKit
+
+class CreateGameViewController: UIViewController {
+    
+    var delegate: UIViewController!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+}
--- SquadUpAustin/MainViewController.swift
+++ SquadUpAustin/MainViewController.swift
@@ -7,14 +7,75 @@
 //
 
 import UIKit
+import CoreData
 
-class MainViewController: UIViewController {
-
+class MainViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    var fetchedGames: [NSManagedObject]!
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return fetchedGames.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cellIdentifier", for: indexPath as IndexPath)
+        let row = indexPath.row
+        cell.textLabel?.numberOfLines = 0
+        cell.textLabel?.text = createMessage(game: fetchedGames[row])
+        return cell
+    }
+    
+    func createMessage(game: NSManagedObject) -> String {
+        let message = "test "
+        let gameTime = game.value(forKey: "time") as! String
+        let gameLocation = game.value(forKey: "location") as! String
+        let sport = game.value(forKey: "sport") as! String
+        return message + gameTime + gameLocation + sport
+    }
+    
+    func retrieveGames() -> [NSManagedObject] {
+        
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let context = appDelegate.persistentContainer.viewContext
+        
+        let request = NSFetchRequest<NSFetchRequestResult>(entityName: "Game")
+        
+        var fetchedResults: [NSManagedObject]? = nil
+        
+        do {
+            try fetchedResults = context.fetch(request) as? [NSManagedObject]
+        } catch {
+            // if an error occurs
+            let nserror = error as NSError
+            NSLog("Unresolved error \(nserror), \(nserror.userInfo)")
+            abort()
+        }
+        
+        return(fetchedResults)!
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    override func viewWillAppear(_ animated: Bool) {
+        fetchedGames = retrieveGames()
+        tableView.reloadData()
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "createGameSegue",
+            let nextVC = segue.destination as? CreateGameViewController
+            {
+                nextVC.delegate = self
+        } else if segue.identifier == "selectGameSegue",
+            let nextVC = segue.destination as? SelectGameViewController
+            {
+                nextVC.delegate = self
+        }
+    }
 }
 
