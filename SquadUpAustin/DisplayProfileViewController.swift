5fb44cec68e500dbc0d5b7940e77e60738a04864
Manuel Ponce <manuelp@wash.cs.utexas.edu>
2020-10-22 17:58:21 -0500

    Connected Firestore to retrieve current users information for profile

--- SquadUpAustin/DisplayProfileViewController.swift
+++ SquadUpAustin/DisplayProfileViewController.swift
@@ -7,17 +7,25 @@
 //
 
 import UIKit
-import Firebase
+import FirebaseAuth
+import FirebaseFirestore
 
 class DisplayProfileViewController: UIViewController {
 
     @IBOutlet weak var ProfileImage: UIImageView!
     @IBOutlet weak var settingsButton: UIButton!
     
+    @IBOutlet weak var usernameLabel: UILabel!
+    @IBOutlet weak var majorLabel: UILabel!
+    @IBOutlet weak var hometownLabel: UILabel!
+    @IBOutlet weak var favoriteSportLabel: UILabel!
+    
+    
     
     let settingsIdentifier = "EditSettingsIdentifier"
     
     let currentUser = Auth.auth().currentUser!
+    let collectionOfUsers = Firestore.firestore().collection("users")
     
     
     override func viewDidLoad() {
@@ -26,6 +34,7 @@ class DisplayProfileViewController: UIViewController {
         // Do any additional setup after loading the view.
         circleProfilePicture()
         setupSettingsButton()
+        populateProfileSettings()
         
     }
     
@@ -44,7 +53,24 @@ class DisplayProfileViewController: UIViewController {
     }
     
     func populateProfileSettings() {
-        
+        let curUser = collectionOfUsers.document(currentUser.uid)
+
+        curUser.getDocument { (document, error) in
+            if let document = document, document.exists {
+                //let dataDescription = document.data().map(String.init(describing:)) ?? "nil"
+                let userData = document.data()!
+                
+                //populate the user information.
+                print("hometown: \(userData["hometown"] ?? "error")")
+                
+                self.usernameLabel.text = (userData["username"] as! String)
+                self.majorLabel.text = (userData["major"] as! String)
+                self.hometownLabel.text = (userData["hometown"] as! String)
+                self.favoriteSportLabel.text = (userData["favoriteSport"] as! String)
+            } else {
+                print("User does not exist")
+            }
+        }
     }
     
     @IBAction func settingButtonPressed(_ sender: Any) {
