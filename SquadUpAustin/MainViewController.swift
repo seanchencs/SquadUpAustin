208eae678301bd68e649ef7fbd21fd936bc819ab
Sean Chen <sean.chen@utexas.edu>
2020-11-01 21:25:07 -0600

    refactored Firestore code

--- SquadUpAustin/MainViewController.swift
+++ SquadUpAustin/MainViewController.swift
@@ -56,6 +56,8 @@ class MainViewController: UIViewController, UITableViewDelegate, UITableViewData
         
         let delete = UIContextualAction(style: .destructive, title: "Delete") { (action, view, completion) in
             self.deleteGame(game: self.fetchedGames[indexPath.row])
+            self.fetchedGames.remove(at: indexPath.row)
+            self.tableView.reloadData()
             completion(true)
         }
         
@@ -77,6 +79,7 @@ class MainViewController: UIViewController, UITableViewDelegate, UITableViewData
     }
     
     //MARK: Firestore
+    /// Store a given game in Firestore
     func storeGame(game: inout Game) {
         var ref: DocumentReference? = nil
         ref = db.collection("games").addDocument(data: game.getDictionary()) { err in
@@ -89,40 +92,25 @@ class MainViewController: UIViewController, UITableViewDelegate, UITableViewData
         game.id = ref!.documentID
     }
     
+    /// Delete a given game from Firestore
     func deleteGame(game:Game) {
-        db.collection("games").getDocuments() { (querySnapshot, err) in
+        db.collection("games").document(game.id!).delete() { err in
             if let err = err {
-                print("Error getting documents: \(err)")
+                print("Error removing document: \(err)")
             } else {
-                self.fetchedGames.removeAll()
-                for document in querySnapshot!.documents {
-                    let data = document.data()
-                    if document.documentID == game.id {
-                        self.db.collection("games").document(document.documentID).delete() { err in
-                            if let err = err {
-                                print("Error removing document: \(err)")
-                            } else {
-                                print("Document successfully removed!")
-                            }
-                        }
-                    } else {
-                        let newGame = Game(id: document.documentID, sport: data["sport"] as! String, location: data["location"] as! String, time: data["time"] as! String, gameOwner: data["gameOwner"] as! String, players: (data["players"] as! String).components(separatedBy: ","), equipmentCheck: data["equipment"] as! Bool)
-                        self.fetchedGames.append(newGame)
-                    }
-                }
-                self.tableView.reloadData()
+                print("Document successfully removed!")
             }
         }
     }
     
-    /// RSVPs  user for game
+    /// RSVPs given user for given game
     func rsvpGame(displayName: String, game: inout Game) {
         game.players.append(displayName)
         let gameRef = db.collection("games").document(game.id!)
         gameRef.setData(game.getDictionary())
     }
     
-    /// UnRSVP user for game
+    /// UnRSVP given user for  given game
     func unRSVP(displayName: String, game: inout Game) {
         let index = game.players.firstIndex(of: displayName) ?? -1
         print(game.players)
@@ -134,7 +122,22 @@ class MainViewController: UIViewController, UITableViewDelegate, UITableViewData
         }
     }
     
-    //fetches all games from firestore and updates tableView
+    // Updates a given game with Firestore version
+    func updateGame(game: inout Game){
+        var newGame: Game?
+        let gameRef = db.collection("games").document(game.id!)
+        gameRef.getDocument { (document, error) in
+            if let document = document, document.exists {
+                let data = document.data()!
+                newGame = Game(id: document.documentID, sport: data["sport"] as! String, location: data["location"] as! String, time: data["time"] as! String, gameOwner: data["gameOwner"] as! String, players: (data["players"] as! String).components(separatedBy: ","), equipmentCheck: data["equipment"] as! Bool)
+            } else {
+                print("Document does not exist")
+            }
+        }
+        game = newGame!
+    }
+    
+    // Completely replaces fetchedGames with Firestore
     func fetchGames() {
         db.collection("games").getDocuments() { (querySnapshot, err) in
             if let err = err {
