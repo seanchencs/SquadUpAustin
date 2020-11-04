8970215a14aaf94482298fcd4c180c85ad176af4
Sean Chen <sean.chen@utexas.edu>
2020-11-04 15:38:46 -0600

    rsvp button on select page

--- SquadUpAustin/SelectGameViewController.swift
+++ SquadUpAustin/SelectGameViewController.swift
@@ -7,6 +7,9 @@
 //
 
 import UIKit
+import Firebase
+import FirebaseAuth
+
 
 class SelectGameViewController: UIViewController {
     
@@ -18,6 +21,7 @@ class SelectGameViewController: UIViewController {
     @IBOutlet weak var locationLabel: UILabel!
     @IBOutlet weak var timeLabel: UILabel!
     @IBOutlet weak var participantsLabel: UILabel!
+    @IBOutlet weak var rsvpButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -27,8 +31,24 @@ class SelectGameViewController: UIViewController {
         locationLabel.text = selectedGame.location
         timeLabel.text = selectedGame.time
         participantsLabel.text = selectedGame.players.joined(separator: ", ")
+        
+        var isOwner = false
+        var RSVPed = false
+        if Auth.auth().currentUser != nil {
+            if Auth.auth().currentUser?.displayName == self.selectedGame.gameOwner {
+                isOwner = true
+            }
+            if self.selectedGame.players.firstIndex(of: ((Auth.auth().currentUser?.displayName) ?? Auth.auth().currentUser?.email?.components(separatedBy: "@")[0])!) != nil {
+                RSVPed = true
+            }
+        }
+        rsvpButton.setTitle(!isOwner ? (RSVPed ? "Cancel RSVP" : "RSVP"): "Delete", for: .normal)
+        rsvpButton.setTitleColor(!isOwner ? (RSVPed ? UIColor.systemRed : UIColor.systemGreen): UIColor.systemRed, for: .normal)
     }
-
+    
+    @IBAction func rsvpButtonPressed(_ sender: Any) {
+        
+    }
     /*
     // MARK: - Navigation
 
