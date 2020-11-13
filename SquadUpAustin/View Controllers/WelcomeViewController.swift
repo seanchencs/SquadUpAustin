ccc6687be50216c43bab6109b2f37fa1e35d0fa5
Sean Chen <seanchen@cs.utexas.edu>
2020-11-13 13:32:52 -0600

    skip the sign in pages if already logged in

--- SquadUpAustin/View Controllers/WelcomeViewController.swift
+++ SquadUpAustin/View Controllers/WelcomeViewController.swift
@@ -16,15 +16,21 @@ class WelcomeViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
+    override func viewDidAppear(_ animated: Bool) {
+        if Auth.auth().currentUser != nil {
+            performSegue(withIdentifier: "alreadyLoggedInSegue", sender: nil)
+        }
+    }
 
-    /*
+    
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        if segue.identifier == "alreadyLoggedInSegue" {
+            segue.destination.modalPresentationStyle = .fullScreen
+        }
     }
-    */
+    
 
 }
